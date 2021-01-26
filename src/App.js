402ad9b44fb27969fd9c8999e0f93c85df1f22d2
taoyuchuan <taoyuchuan@gmail.com>
2021-01-25 19:45:50 -0500

    Add get request using axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import PostList from './components/PostList'
 
 class App extends Component {
   render() {
     return(
-      <div>
-        Hello
+      <div className = 'App'>
+        <PostList />
       </div>
     )
   }
